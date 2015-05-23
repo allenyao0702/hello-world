5b24e6efd51fcfa9b6984117c2c1e082d951d91c
allenyao0702 <allenyao0702@gmail.com>
2015-05-23 12:50:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import sys
 class User(object):
     
-    
+    def __init__(self):
+        pass
     userId=-1
     followers_id=[]
 
@@ -11,3 +12,35 @@ class User(object):
 
     def Remove_followers(self, followerid):
         self.followers_id.remove(followerid)
+
+class Photo(object):
+    
+    def __init__(self):
+        pass
+    photoID=-1
+    url=''
+    userId=-1
+    CommentList=[]
+
+    def Add_Comments(self, commentId):
+        CommentList.append(item)
+
+    def Remove_Comments(self, commentId):
+        commentList.remove(commentId)
+
+class PhotoList(object):
+    
+    def __init__(self):
+        pass
+
+    userId=-1
+    PhotoList=[]
+
+    def Add_photo(self, photoid):
+        PhotoList.append(photoid);
+
+    def Remove_photo(self, photoid):
+        photoList.remove(photoid)
+
+
+
